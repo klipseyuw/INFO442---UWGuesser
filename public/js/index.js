1c7ef8b350d61ae8dd1565ced72ef26cc2709511
klipsey <kentalipsey@gmail.com>
2025-05-22 15:09:58 -0700

    Cleaned up win condition and button events

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
 let panorama;
+let score;
+let currentBuildingIndex;
+let clickListener;
+let dubsMarker;
 
 // Format: Street View coords, marker to guess coords
 const buildings = 
@@ -6,10 +10,27 @@ const buildings =
 [{lat: 47.6561734, lng: -122.3093858}, {name: "Kane Hall", lat: 47.6567387, lng: -122.3092925}], 
 [{lat: 47.6559409, lng: -122.3075026}, {name: "Suzzalo Library", lat: 47.6557702, lng: -122.3078427}]]
 
-function initMap() {
-    const max = buildings.length;
-    const rand = Math.floor(Math.random() * max);
-    const astorPlace = buildings[rand][0];
+let buildingsInstance;
+
+function initMap()
+{
+    reset();
+    setNextMap();
+}
+
+function setNextMap() {
+    document.getElementById("answerText").value = "";   
+    if(buildingsInstance.length == 0)
+    {
+        document.getElementById("header").innerHTML = "You Win!";
+        return;
+    }
+
+    const max = buildingsInstance.length;
+    
+    currentBuildingIndex = Math.floor(Math.random() * max);
+
+    const astorPlace = buildingsInstance[currentBuildingIndex][0];
     // Set up the map
     const map = new google.maps.Map(document.getElementById("map"), {
     center: astorPlace,
@@ -24,16 +45,17 @@ function initMap() {
     "https://developers.google.com/maps/documentation/javascript/examples/full/images/cafe_icon.svg";
 
     // Set up the markers on the map
-    const cafeMarker = new google.maps.Marker({
-        position: buildings[rand][1],
+    dubsMarker = new google.maps.Marker({
+        position: buildingsInstance[currentBuildingIndex][1],
         map,
         title: "BuildingToGuess",
         icon: cafeIcon.src,
-        gmpClickable: true
+        gmpClickable: true,
     });
 
+    clickListener = dubsMarker.addListener("click", addToScore);
 
-    cafeMarker.addListener("click", initMap);
+    dubsMarker.setMap(map);
 
     // We get the map's default panorama and set up some defaults.
     // Note that we don't yet set it visible.
@@ -54,8 +76,36 @@ function initMap() {
     panorama.setVisible(true);
 }
 
-function test() {
-    console.log("Clicked");
+function addToScore() {
+    score++;
+    document.getElementById("dubsScore").innerHTML = "Dubs Score: " + score;
+    dubsMarker.setMap(null);
+}
+
+function submitAnswer()
+{
+    var ans = document.getElementById("answerText").value;
+    
+    if(buildingsInstance[currentBuildingIndex][1].name.includes(ans))
+    {
+        removeBuilding(currentBuildingIndex);
+        setNextMap();
+    }
+    else
+    {
+        document.getElementById("answerText").value = "Wrong Answer!";
+    }
+}
+
+function reset()
+{
+    buildingsInstance = buildings;
+    score = 0;
+}
+
+function removeBuilding(index)
+{
+    buildingsInstance.splice(index, 1);
 }
 
 window.initMap = initMap;
