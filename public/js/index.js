88bb7a89ee65751352d0c104a8e3bef5fbfca3ab
klipsey <kentalipsey@gmail.com>
2025-05-22 14:11:13 -0700

    Randomizing, dictionary formatting, icon to guess complete

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,63 @@
-const startScreen = document.getElementById('startScreen');
+let panorama;
 
-const buildings = []; 
+// Format: Street View coords, marker to guess coords
+const buildings = 
+[[{lat: 47.6557022, lng: -122.3045488}, {name: "HUB", lat: 47.6553893, lng: -122.3050845}], 
+[{lat: 47.6561734, lng: -122.3093858}, {name: "Kane Hall", lat: 47.6567387, lng: -122.3092925}], 
+[{lat: 47.6559409, lng: -122.3075026}, {name: "Suzzalo Library", lat: 47.6557702, lng: -122.3078427}]]
 
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-function initialize() {
-const fenway = { lat: 42.345573, lng: -71.098326 };
-const map = new google.maps.Map(document.getElementById("map"), {
-    center: fenway,
-    zoom: 14,
-});
-const panorama = new google.maps.StreetViewPanorama(
-    document.getElementById("pano"),
-    {
-    position: fenway,
-    pov: {
-        heading: 34,
-        pitch: 10,
+function initMap() {
+    const max = buildings.length;
+    const rand = Math.floor(Math.random() * max);
+    const astorPlace = buildings[rand][0];
+    // Set up the map
+    const map = new google.maps.Map(document.getElementById("map"), {
+    center: astorPlace,
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true
+    });
+
+    const cafeIcon = document.createElement("img");
+
+    cafeIcon.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/cafe_icon.svg";
+
+    const dollarIcon = document.createElement("img");
+
+    dollarIcon.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/bank_icon.svg";
+
+    const busIcon = document.createElement("img");
+
+    busIcon.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/bus_icon.svg";
+
+    // Set up the markers on the map
+    const cafeMarker = new google.maps.Marker({
+    position: buildings[rand][1],
+    map,
+    title: "BuildingToGuess",
+    icon: cafeIcon.src,
+    });
+
+    // We get the map's default panorama and set up some defaults.
+    // Note that we don't yet set it visible.
+    panorama = map.getStreetView(); // TODO fix type
+    panorama.setPosition(astorPlace);
+    panorama.setPov(
+    /** @type {google.maps.StreetViewPov} */ {
+        heading: 265,
+        pitch: 0
     },
+    );
+    panorama.setOptions(
+    {
+        enableCloseButton: false,
+        disableDefaultUI: true,
     }
-);
-
-map.setStreetView(panorama);
+    )
+    panorama.setVisible(true);
 }
 
-window.initialize = initialize;
+window.initMap = initMap;
